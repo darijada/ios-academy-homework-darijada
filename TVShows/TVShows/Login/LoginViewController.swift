84f4a03e28515ede5ba5b95a5098e96a82072c40
Darija Dabic <darija0@yahoo.com>
2019-07-05 17:39:19 +0200

    Activity Indicator View added.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -13,20 +13,35 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var myLabel: UILabel!
     
-    // numer of clicks stored in counter
+    @IBOutlet weak var startButton: UIButton!
     
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
+    
+    // numer of clicks stored in counter
     var counter = 0
     
     
-    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
         myLabel.text = String(counter)
         
+        
+        // Activity Indicator View - automatic start when the view appears and stop 3 seconds later
+        
+        myActivityIndicator.startAnimating()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+            self.myActivityIndicator.stopAnimating()
+        }
+        
+        
+        
     }
+    
     
     @IBAction func myButton(_ sender: UIButton, forEvent event: UIEvent) {
         
@@ -36,8 +51,18 @@ class LoginViewController: UIViewController {
     }
     
     
+    @IBAction func startAIV(_ sender: UIButton, forEvent event: UIEvent) {
+        
+        myActivityIndicator.startAnimating()
+    }
+    
  
+    @IBAction func stopAIV(_ sender: UIButton){
+       
+        myActivityIndicator.stopAnimating()
 
+    }
+    
     /*
     // MARK: - Navigation
 
