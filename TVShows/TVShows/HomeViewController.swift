615d66fa0b9918052d319ed1503c6dccc1482cb3
Darija Dabic <darija0@yahoo.com>
2019-07-16 00:08:52 +0200

    Homework 3 and 4 finished.

--- TVShows/TVShows/HomeViewController.swift
+++ TVShows/TVShows/HomeViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -11,15 +11,16 @@ import SVProgressHUD
 import Alamofire
 import CodableAlamofire
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
     
     // MARK: - Outlets
     
     @IBOutlet private weak var logInButton: UIButton!
     @IBOutlet private weak var createAccountButton: UIButton!
     @IBOutlet private weak var checkmarkButton: UIButton!
-    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var emailTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
+    private var _infoLabel: String = "Unknown"
     
     // MARK: - Lifecycle methods
     
@@ -32,17 +33,88 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
     // MARK: - Actions
     
     @IBAction private func logInButtonActionHandler() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
         let viewController = storyboard.instantiateViewController( withIdentifier: "HomeViewController")
         navigationController?.pushViewController(viewController, animated: true)
+        
+        _loginUserWith(email: emailTextField.text!, password: passwordTextField.text!)
     }
-    
-    @IBAction func createAccountButtonActionHandler() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+    @IBAction private func createAccountButtonActionHandler() {
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
         let viewController = storyboard.instantiateViewController( withIdentifier: "HomeViewController")
         navigationController?.pushViewController(viewController, animated: true)
+        
+        _alamofireCodableRegisterUserWith(email: emailTextField.text!, password: passwordTextField.text!)
+    }
+}
+
+    // MARK: - Register + automatic JSON parsing
+
+private extension LoginViewController {
+    
+    func _alamofireCodableRegisterUserWith(email: String, password: String) {
+        SVProgressHUD.show()
+        
+        let parameters: [String: String] = [
+            "email": email,
+            "password": password
+        ]
+        
+        Alamofire
+            .request(
+                "https://api.infinum.academy/api/users",
+                method: .post,
+                parameters: parameters,
+                encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) { (response: DataResponse<User>) in
+                switch response.result {
+                case .success(let user):
+                   print("Success: \(user)")
+                    SVProgressHUD.showSuccess(withStatus: "User registered!")
+                    
+                case .failure(let error):
+                    print("API failure: \(error)")
+                }
+        }
+    }
+}
+
+    // MARK: - Login + automatic JSON parsing
+
+private extension LoginViewController {
+    
+    func _loginUserWith(email: String, password: String) {
+        SVProgressHUD.show()
+        let parameters: [String: String] = [
+            "email": email,
+            "password": password
+        ]
+        Alamofire
+            .request(
+                "https://api.infinum.academy/api/users/sessions",
+                method: .post,
+                parameters: parameters,
+                encoding: JSONEncoding.default)
+            .validate()
+            .responseJSON { [weak self] dataResponse in
+                switch dataResponse.result {
+                case .success(let response):
+                    self?._infoLabel = "Success: \(response)"
+                    SVProgressHUD.showSuccess(withStatus: "Successful login!")
+                case .failure(let error):
+                    self?._infoLabel = "API failure: \(error)"
+                    SVProgressHUD.showError(withStatus: "Incorrect Email or Password!")
+                }
+        }
     }
 }
